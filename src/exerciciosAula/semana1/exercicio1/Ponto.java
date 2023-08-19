87ae31badc2afbc49363b488b12b6969e895f85c
Ana Beatriz Stahl <StahlAna@edu.unisinos.br>
2023-08-18 22:03:37 -0300

    inicio dos exercicios

---
+++ src/exerciciosAula/semana1/exercicio1/Ponto.java
@@ -0,0 +1,17 @@
+package exerciciosAula.semana1.exercicio1;
+
+public class Ponto {
+    // Atributos
+    private double x;
+    private double y;
+
+    // Construtor
+    public Ponto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Métodos
+
+    // Get e Set
+}

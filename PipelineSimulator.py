759598ebfc62d7dcec2bc50c032a237303d6c94d
Ana Beatriz Stahl <ana.beatriz.stahl@gmail.com>
2024-06-05 09:50:35 -0300

    feat: rename some variables and add some comments

--- PipelineSimulator.py
+++ PipelineSimulator.py
@@ -3,16 +3,16 @@ from Instruction import Instruction
 class PipelineSimulator:
     def __init__(self, program_file):
         self.instructions = self.import_program_file(program_file)
-        self.labels = self.find_labels(program_file)
-        self.memory_data = self.load_memory(program_file)
+        self.labels = self.find_labels(program_file) # Save label references on the code
+        self.memory_data = self.load_memory(program_file) # Simulate the data in Memory
         self.pc = 0  # Program counter
         self.totalRuns = 0 # Count how many runs were make
-        self.fetchStep = None  # Instruction fetched (IF/ID pipeline register)
-        self.decodeStep = None  # Instruction decoded (ID/EX pipeline register)
-        self.executeStep = None  # Instruction executed (EX/MEM pipeline register)
-        self.memoryStep = None  # Instruction memory accessed (MEM/WB pipeline register)
-        self.wb_value = None # Write-back result
-        self.registers = {f'{i}': 0 for i in range(32)}  # Register file with 32 registers
+        self.fetchedStep = None  # Instruction fetched
+        self.decodedStep = None  # Instruction decoded
+        self.executedStep = None  # Instruction executed
+        self.memoryStep = None  # Instruction memory accessed
+        self.wb_value = None # Write-back value
+        self.registers = {f'{i}': 0 for i in range(32)}  # Simulate the register file with 32 registers
 
     def import_program_file(self, entire_file):
         instructions_list = []
@@ -21,9 +21,11 @@ class PipelineSimulator:
         for line in lines:
             instruction_parts = line.split()
             if instruction_parts:
-                if instruction_parts[0].endswith(":"):
+                if instruction_parts[0] == "loop:":
                     opcode = instruction_parts[1]
                     operands = instruction_parts[2:]
+                elif instruction_parts[0].endswith(":"):
+                    continue
                 else:
                     opcode = instruction_parts[0]
                     operands = instruction_parts[1:]
@@ -42,7 +44,7 @@ class PipelineSimulator:
             if parts[0].endswith(":"):
                 label = parts[0][:-1]
                 labels[label] = idx
-        print(labels)
+        
         return labels
 
     def load_memory(self, entire_file):
@@ -59,42 +61,42 @@ class PipelineSimulator:
                     memory[idx] = self.labels[value]
                 else:
                     memory[idx] = 0  # Default to 0 if not found
-        print(memory)
+        
         return memory
 
     def fetch(self):
         if self.pc < len(self.instructions):
-            self.fetchStep = self.instructions[self.pc]
+            self.fetchedStep = self.instructions[self.pc]
             self.pc += 1
         else:
-            self.fetchStep = None
+            self.fetchedStep = None
 
     def decode(self):
-        if self.fetchStep:
-            self.decodeStep = self.fetchStep
-            if self.decodeStep.opcode in ["add", "addi", "beq", "noop", "halt", "lw"]:
-                self.decodeStep.validate = True
+        if self.fetchedStep:
+            self.decodedStep = self.fetchedStep
+            if self.decodedStep.opcode in ["add", "addi", "beq", "noop", "halt", "lw"]:
+                self.decodedStep.validate = True
             else:
-                self.decodeStep.validate = False
+                self.decodedStep.validate = False
         else:
-            self.decodeStep = None
+            self.decodedStep = None
     
     def execute(self):
-        if self.decodeStep and self.decodeStep.validate:
-            if self.decodeStep.opcode == "add":
-                self.wb_value = (self.decodeStep.op1, self.registers[self.decodeStep.op2] + self.registers[self.decodeStep.op3])
-            elif self.decodeStep.opcode == "beq":
-                if self.registers[self.decodeStep.op1] == self.registers[self.decodeStep.op2]:
-                    self.pc = self.labels[self.decodeStep.op3] # TODO
-            elif self.decodeStep.opcode == "halt":
+        if self.decodedStep and self.decodedStep.validate:
+            if self.decodedStep.opcode == "add":
+                self.wb_value = (self.decodedStep.op1, self.registers[self.decodedStep.op2] + self.registers[self.decodedStep.op3])
+            elif self.decodedStep.opcode == "beq":
+                if self.registers[self.decodedStep.op1] == self.registers[self.decodedStep.op2]:
+                    self.pc = self.labels[self.decodedStep.op3] # TODO
+            elif self.decodedStep.opcode == "halt":
                 self.pc = len(self.instructions)  # Force end of instructions
-            self.executeStep = self.decodeStep
+            self.executedStep = self.decodedStep
         else:
-            self.executeStep = None
+            self.executedStep = None
     
     def memory_access(self):
-        if self.executeStep and self.executeStep.validate:
-            self.memoryStep = self.executeStep
+        if self.executedStep and self.executedStep.validate:
+            self.memoryStep = self.executedStep
         else:
             self.memoryStep = None
 
@@ -110,15 +112,15 @@ class PipelineSimulator:
 
     def print_pipeline_state(self):
         print(f"\n\033[1m--- Pipeline State | PC: {self.pc} | Total Runs: {self.totalRuns} ---\033[0m")
-        print(f"Fetch Step: {self.fetchStep}")
-        print(f"Decode Step: {self.decodeStep}")
-        print(f"Execution Step: {self.executeStep}")
-        print(f"Memory Step: {self.memoryStep}")
+        print(f"IF/ID: {self.fetchedStep}")
+        print(f"ID/EX: {self.decodedStep}")
+        print(f"EX/ME: {self.executedStep}")
+        print(f"ME/WB: {self.memoryStep}")
         print(f"Registers: {self.registers}")
         print(f"Memories: {self.memory_data}")
 
     def run(self):
-        while self.pc < len(self.instructions) or any([self.fetchStep, self.decodeStep, self.executeStep, self.memoryStep]):
+        while self.pc < len(self.instructions) or any([self.fetchedStep, self.decodedStep, self.executedStep, self.memoryStep]):
             input("\n\033[32mPress Enter to continue...\033[0m")
             self.write_back()
             self.memory_access()

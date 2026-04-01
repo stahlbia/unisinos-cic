f03649ca2d57e0e94e12397a7083eaf62b9f65e5
Ana Beatriz <ana.beatriz.stahl@gmail.com>
2026-03-31 21:31:59 -0300

    chore: change variable on set path script

--- SetPath.bat
+++ SetPath.bat
@@ -1,3 +1,3 @@
-set PATH=%PATH%;C:\msys64\ucrt64\bin;C:\Users\rossanaqueiroz\Documents\cmake-4.0.2-windows-x86_64\bin;C:\Users\rossanaqueiroz\Documents\PortableGit\bin
+set PATH=%PATH%;C:\msys64\ucrt64\bin;C:\Users\stahlana\Documents\cmake-4.0.2-windows-x86_64\bin;C:\Users\stahlana\Documents\PortableGit\bin
 
 code .
